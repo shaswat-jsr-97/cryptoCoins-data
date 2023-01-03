cf194b061cc5aefded85818211114c3c69e5fb1a
shaswat-jsr-97 <shaswatjsr97work@gmail.com>
2023-01-03 20:09:39 +0530

    feat(cryptocoins):added all files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/Footer.css"
+
+
+const Footer = () => {
+  return (
+    <div id='signature'>
+        Made by Shaswat Sinha
+    </div>
+  )
+}
+
+export default Footer
